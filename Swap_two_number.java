0b14319377d024478c452abdd0120fce0ce6de2c
Akhil Soni <85015174+Akhil9368@users.noreply.github.com>
2022-07-28 17:58:47 +0530

    Create Swap_two_number.java

---
+++ Swap_two_number.java
@@ -0,0 +1,9 @@
+
+public class Even_Or_Oddd {
+    public static void main(String[] args) {
+      int x  = 10;
+      int y = 20;
+      x= x^y^(y=x);
+        System.out.println(x +" " + y);
+    }
+}
