e2bef1dd36383e46892106c7f2eb4f7c139c189e
Akhil Soni <85015174+Akhil9368@users.noreply.github.com>
2022-07-28 15:00:33 +0530

    Check both the numbers are equals or not

---
+++ Equal_or_not.java
@@ -0,0 +1,12 @@
+// Perform xor operation without using xor(operator)
+public class Equal_or_Not {
+    public static void main(String[] args) {
+     int x =10;
+     int y =10;
+     if( (x^y) == 0){
+         System.out.println("Both are Equals...");
+     }
+     else
+         System.out.println("Both are different....");
+    }
+}
