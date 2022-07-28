a80d24e6bbc8c066c9728984a83f91d0a2d28635
Akhil Soni <85015174+Akhil9368@users.noreply.github.com>
2022-07-28 17:05:19 +0530

    Update Powe_of_two.java

--- Powe_of_two.java
+++ Powe_of_two.java
@@ -1,1 +1,17 @@
-
+public class Even_Or_Oddd {
+    public static void main(String[] args) {
+        int n=8;
+        int count =0;
+        while(n>0){
+            if((n & 1) == 1){
+                count++;
+            }
+            n=n>>1;
+        }
+       if(count ==1){
+           System.out.println("Yes it's Power of two");
+       }
+       else
+           System.out.println("No it's not the power of two");
+    }
+}
