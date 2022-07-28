3b1f6d27ecd33dbcbbe774314a1b1cb5323bd4c5
Akhil Soni <85015174+Akhil9368@users.noreply.github.com>
2022-07-28 12:36:15 +0530

    Create Check odd_Or Even.java

---
+++ Check odd_Or Even.java
@@ -0,0 +1,10 @@
+public class Even_Or_Oddd {
+    public static void main(String[] args) {
+        int x= 1;
+        if((x & 1)==0){
+            System.out.println("It's Even Number....");
+        }
+        else
+            System.out.println("It's a odd Number");
+    }
+}
